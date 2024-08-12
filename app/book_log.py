9ecbc3863c7af277b0658def38c7fb957d89b62e
unalivij <jed.unalivia@lexisnexisrisk.com>
2024-08-13 05:46:45 +0800

    added views

--- app/book_log.py
+++ app/book_log.py
@@ -13,7 +13,7 @@ bp = Blueprint('book_log', __name__)
 def index():
     db = get_db()
     book_logs = db.execute(
-        "SELECT book_log.id, datetime_log, remarks, book_status, full_name, title, author"
+        "SELECT book_log.id, datetime_log, remarks, book_status, user_id, book_id, full_name, title, author"
         " FROM book_log JOIN user ON book_log.user_id = user.id JOIN book ON book_log.book_id = book.id"
         " ORDER BY datetime_log DESC"
     ).fetchall()
@@ -62,6 +62,7 @@ def get_book_details(book_id):
     return book
 
 @bp.route('/edit_book_details/<int:book_id>', methods=("GET", "POST"))
+@login_required
 def edit_book_details(book_id):
     book = get_book_details(book_id)
     
@@ -90,7 +91,40 @@ def edit_book_details(book_id):
                 (book_dict['ISBN'], book_dict['Title'], book_dict['Author'], book_dict['Category'], book_desc, book_id)
             )
             db.commit()
-            return redirect(url_for('book_log.index'))
+            return redirect(url_for('book_log.list_books'))
 
     return render_template('book_log/edit_book.html', book=book)
 
+@bp.route('/books')
+def list_books():
+    all_books = get_db().execute(
+        'SELECT * FROM book'
+    ).fetchall()
+
+    return render_template('book_log/books.html', all_books=all_books)
+
+@bp.route('/log_entry/<int:book_id>', methods=('GET', 'POST'))
+@login_required
+def enter_log(book_id):
+    book_info = get_db().execute(
+        "SELECT * FROM book WHERE id = ?", (book_id,)
+    ).fetchone()
+    if request.method == 'POST':
+        book_status = request.form['book_status']
+        book_remarks = request.form['book_remarks']
+        
+        error = None
+        if not book_status:
+            error = "Status is required."
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO book_log (remarks, book_status, user_id, book_id) VALUES (?, ?, ?, ?)',
+                (book_remarks, book_status, g.user['id'], book_info['id'])
+            )
+            db.commit()
+            return redirect(url_for('book_log.index'))
+
+    return render_template('book_log/log_entry.html', book_info=book_info)
