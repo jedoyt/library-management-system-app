6a641718c861f2f8fd7d4e543fd6f15f41bb0d21
unalivij <jed.unalivia@lexisnexisrisk.com>
2025-03-29 22:20:16 +0800

    objects.py: Added new categories-- Booklets & Periodics, Children's Books, Doctrine Studies

--- app/objects.py
+++ app/objects.py
@@ -3,11 +3,12 @@ book_status_list = ["Available", "Borrowed", "Returned", "Damaged", "Lost", "Una
 
 # Book Categories
 categories = [
-        "Apologetics", "Bibles", "Biblical Theology", "Biographies", "Christian Classics", 
-        "Christian Living", "Church History", "Church Ministry", "Collected Works", "Commentaries", 
-        "Creeds and Confessions", "Devotionals", "Dictionaries, Concordance, etc.", "IX 9 Marks Series", 
+        "Apologetics", "Bibles", "Biblical Theology", "Biographies", "Booklets & Periodics",
+        "Children's Books", "Christian Classics",  "Christian Living", "Church History", 
+        "Church Ministry", "Collected Works", "Commentaries", "Creeds and Confessions", 
+        "Devotionals", "Dictionaries, Concordance, etc.", "Doctrine Studies", "IX 9 Marks Series", 
         "Liturgy Worship", "New Testament Studies", "Old Testament Studies", "Pastoral Ministry", 
-        "Puritan Works", "Study Bibles", "Systematic Theology", "The 5 Solas Series", "Theology",
+        "Puritan Works", "Study Bibles", "Systematic Theology", "The 5 Solas Series", "Theology", 
         "Uncategorized"
     ]
 
